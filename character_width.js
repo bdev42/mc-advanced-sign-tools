e10d86df294eb245964098c4ead0e09b3ce26eb8
bdev42 <140114350+bdev42@users.noreply.github.com>
2025-08-01 19:36:52 +0200

    Various improvements: Added progress bar, results are now continuously displayed and scrolled to, fixed exotic unicode chars being counted as multiple different chars

--- character_width.js
+++ character_width.js
@@ -275,7 +275,8 @@ function redraw_bitmaps_list() {
         const i = document.createElement("img");
         i.src = "textures/" + bitmap.file;
         const t = document.createElement("table");
-        d.append(s, i, t);
+        const p = document.createElement("progress");
+        d.append(s, i, t, p);
 
         d.addEventListener("toggle", (e) => process_bitmap(e, bitmap));
         output.appendChild(d);
@@ -283,7 +284,7 @@ function redraw_bitmaps_list() {
 }
 
 
-function process_bitmap(toggle_event, bitmap) {
+async function process_bitmap(toggle_event, bitmap) {
     const details = toggle_event.target;
     if (!details.open) return;
     
@@ -298,14 +299,19 @@ function process_bitmap(toggle_event, bitmap) {
     const table = details.querySelector("table");
     table.innerHTML = "<tr><th>unicode</th><th>char</th><th>img</th><th>width</th></tr>";
 
-
     // Find the number of rows and columns of characters in the atlas
     let atlas_width = 0;
     for(let chars_line of bitmap.chars) {
-        if (chars_line.length > atlas_width) atlas_width = chars_line.length;
+        const len = [...chars_line].length;
+        if (len > atlas_width) atlas_width = len;
     }
     let atlas_height = bitmap.chars.length;
     console.debug(`Atlas layout: ${atlas_width} x ${atlas_height} characters`)
+
+    // Set progressbar target
+    const progress = details.querySelector("progress");
+    progress.max = atlas_height * atlas_width;
+    progress.value = 0;
 
     // Setup atlas on canvas for analysis
     const canvas = document.createElement('canvas');
@@ -315,15 +321,14 @@ function process_bitmap(toggle_event, bitmap) {
     ctx.drawImage(img, 0, 0);
     console.debug(`Atlas dimensions: ${img.width} x ${img.height} pixels`)
 
-
     const tex_width = Math.floor(img.width / atlas_width);
     const tex_height = Math.floor(img.height / atlas_height);
     console.debug(`Character texture dimensions: ${tex_width} x ${tex_height} pixels`)
 
     // Analyze texture of all non-"null" characters
     for (let cy = 0; cy < atlas_height; cy++) {
-        for (let cx = 0; cx < atlas_width && cx < bitmap.chars[cy].length; cx++) {
-            const char = bitmap.chars[cy][cx];
+        for (let cx = 0; cx < atlas_width && cx < [...bitmap.chars[cy]].length; cx++) {
+            const char = [...bitmap.chars[cy]][cx];
             if (char === "\u0000") { continue; }
 
             // extract real character width
@@ -336,14 +341,19 @@ function process_bitmap(toggle_event, bitmap) {
             glyph.height = img_data.height;
             glyph.getContext('2d').putImageData(img_data, 0, 0);
 
+            // display result
             const res = document.createElement("tr");
-            res.innerHTML = `<td>\\u${char.codePointAt(0).toString(16).padStart(4, '0')}</td><td>${char}</td><td><img alt="Broken ${char} glyph" src="${glyph.toDataURL("image/png")}"></td><td>${real_char_width}</td>`
+            res.innerHTML = `<td>U+${char.codePointAt(0).toString(16).padStart(4, '0')}</td><td>${char}</td><td><img alt="Broken ${char} glyph" src="${glyph.toDataURL("image/png")}"></td><td>${real_char_width}</td>`
             table.appendChild(res);
 
-            console.info(`U${char.codePointAt(0).toString(16).padStart(4, '0')}</td><td>${char}</td><td><img alt="Broken ${char} glyph" src="${glyph.toDataURL("image/png")}"></td><td>${real_char_width}</td>`)
+            progress.value = cy * atlas_width + cx;
+            progress.scrollIntoView(false);
+
+            await new Promise(requestAnimationFrame);
         }
     }
 
+    progress.value = progress.max;
     console.timeEnd("proc_bitmap");
 }
 
