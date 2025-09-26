add033a016c8139251200f0d06fce7f9c364be81
bdev42 <140114350+bdev42@users.noreply.github.com>
2025-09-27 00:04:40 +0200

    Sign Editor improvements: including copy line buttons and length counters as well as new utility tools.

--- sign_editor.js
+++ sign_editor.js
@@ -1,4 +1,5 @@
 import {
+    fb_span,
     load_atlas_map,
     load_char_sizes,
     load_char_textures_map_and_atlas_list
@@ -7,12 +8,14 @@ import {
 const sign_width = 96;
 const sign_height = 48;
 const sign_margin = 3;
+const sign_textarea = sign_width - 2*sign_margin;
 const scalingFactor = 4;
 
 const char_sizes = await load_char_sizes('./out/char_sizes_full.txt');
 const [char_textures, atlas_list] = await load_char_textures_map_and_atlas_list('./font_mappings/default.json');
 const atlas_map = await load_atlas_map(atlas_list);
 
+const form = document.querySelector("#tool-mse form");
 const text = document.querySelector("textarea#mse-input");
 const sign = document.querySelector("canvas#mse-output");
 const background = document.querySelector("img#mse-bg");
@@ -23,13 +26,31 @@ const toggleCentering = document.querySelector("input#mse-enable-centering");
 
 const overlayLegend = document.querySelector("#overlay-legend");
 
+const lineCopyContainer = document.querySelector("#mse-line-copy");
+const lineLengthsContainer = document.querySelector("#mse-line-length");
+
+const btnTruncate = document.querySelector("#mse-btn-truncate");
+const btnBalance = document.querySelector("#mse-btn-balance");
+
 toggleOverlay.addEventListener("input", () => {
     overlayLegend.classList.toggle("hidden", !toggleOverlay.checked);
+});
+
+[...lineCopyContainer.children].forEach((btn, idx) => {
+    btn.addEventListener("click", () => {
+        const line = text.value.split("\n")[idx];
+        if(line) navigator.clipboard.writeText(line);
+    });
 });
 
 [text, toggleBackground, toggleOverlay, toggleCentering].forEach(el => {
     el.addEventListener("input", run_tool);
 })
+
+form.onsubmit = (e) => e.preventDefault();
+btnTruncate.addEventListener("click", utils_truncate);
+btnBalance.addEventListener("click", utils_balance);
+
 /**
  * @type CanvasRenderingContext2D
  */
@@ -39,24 +60,36 @@ ctx.imageSmoothingEnabled = false;
 ctx.lineWidth = 1;
 
 function run_tool() {
-    redraw_sign({
+    const line_lengths = redraw_sign({
+        ctx,
         layer_background: toggleBackground.checked,
         layer_overlay: toggleOverlay.checked,
         center_text: toggleCentering.checked,
         text_tint: "black",
     });
+
+    display_line_lengths(line_lengths);
 }
 run_tool();
 
+function get_lines() {
+    const lines = [];
+    for (const line of text.value.split("\n")) lines.push([...line]);
+    return lines;
+}
 
-function redraw_sign({layer_background, layer_overlay, center_text, text_tint}) {
+function set_lines(lines) {
+    text.value = lines.map(l => l.join("")).join("\n");
+}
+
+function redraw_sign({ctx, layer_background, layer_overlay, center_text, text_tint}) {
     if (!char_sizes.size) {
         ctx.fillText("Loading...", 10, 20);
         return;
     }
+    const line_lengths = [];
 
-    const lines = [];
-    for (const line of text.value.split("\n")) lines.push([...line]);
+    const lines = get_lines();
     // clear canvas
     if (layer_background) ctx.drawImage(background, 0, 0, sign_width, sign_height);
     else ctx.clearRect(0, 0, sign_width, sign_height);
@@ -138,7 +171,7 @@ function redraw_sign({layer_background, layer_overlay, center_text, text_tint}) 
         lineCtx.globalCompositeOperation = "source-over";
         // draw line onto output canvas & center if needed
         let line_start = sign_margin;
-        if (center_text && cursor < sign_width - 2*sign_margin) line_start = Math.round(sign_width/2 - cursor/2);
+        if (center_text && cursor < sign_textarea) line_start = Math.round(sign_width/2 - cursor/2);
         ctx.drawImage(lineCanvas,
             0, 0, sign_width-sign_margin, sign_height,
             line_start, 0, sign_width-sign_margin, sign_height
@@ -149,5 +182,52 @@ function redraw_sign({layer_background, layer_overlay, center_text, text_tint}) 
                 line_start, 0, sign_width-sign_margin, sign_height
             );
         }
+        line_lengths.push(cursor);
     }
+    return line_lengths;
+}
+
+function display_line_lengths(line_lengths) {
+    lineLengthsContainer.innerHTML = "";
+    for (let l = 0; l < 4; l++) {
+        if (l > line_lengths.length-1) {
+            lineLengthsContainer.appendChild(fb_span("-", false));
+        } else if (line_lengths[l] <= sign_textarea) {
+            lineLengthsContainer.appendChild(fb_span(`${line_lengths[l]}/${sign_textarea}`, false));
+        } else {
+            lineLengthsContainer.appendChild(fb_span(`${line_lengths[l] - sign_textarea} >`, true));
+        }
+    }
+}
+
+function utils_truncate() {
+    let truncated = get_lines().slice(0, 4);
+    
+    for (let l = 0; l < truncated.length; l++) {
+        let cursor = 0;
+        for (let c = 0; c < truncated[l].length; c++) {
+            const char = truncated[l][c];
+            if (!char_sizes.has(char)) continue;
+
+            const char_size = char_sizes.get(char);
+
+            if (char === " ") {
+                cursor += char_size.width;
+            } else {
+                cursor += char_size.width+1;
+            }
+
+            if (cursor > sign_textarea) {
+                truncated[l] = truncated[l].slice(0, c);
+                break;
+            }
+        }
+    }
+
+    set_lines(truncated);
+    run_tool();
+}
+
+function utils_balance() {
+    // TODO
 }
